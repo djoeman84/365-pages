66825ef311cb16015ee89aba49238565e628a212
delgadoj <djoeman84@gmail.com>
2013-08-21 11:48:41 -0700

    last last commit before gmaps

--- assets/js/aug_21_game.js
+++ assets/js/aug_21_game.js
@@ -13,7 +13,7 @@ function init_game () {
 
 function query_fb_for_friends () {
 	//get uid, name, hometown,
-	FB.api('/fql?q='+escape('SELECT uid,name,hometown_location FROM user WHERE hometown_location AND uid IN (SELECT uid1 FROM friend WHERE uid2 = me())'),function(r){
+	FB.api('/fql?q='+escape('SELECT uid,name,hometown_location,profile_url FROM user WHERE hometown_location AND uid IN (SELECT uid1 FROM friend WHERE uid2 = me())'),function(r){
 		friend_data = r.data;
 		play_game();
 	});
@@ -32,7 +32,7 @@ function update_display (i) {
 	FB.api('/'+friend_data[i].uid+'?fields=picture.width(200).height(200)', function (r) {
 		setImgSrc('jumbo-friend-pic', r.picture.data.url);
 	});
-	document.getElementById('friend-profile-bottom').innerHTML = friend_data[i].name;
+	document.getElementById('friend-profile-anchor').innerHTML = friend_data[i].name;
 	i++;
 	if (i === friend_data.length) {
 		shuffle(friend_data);
